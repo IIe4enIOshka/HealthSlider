d90151a80ab9d07647fdf92143bf60fbe76fe0c8
Алексей Выродов <g47711@gmail.com>
2022-02-10 15:11:08 +0700

    Create project

---
+++ Assets/Scripts/DisplayHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayHealth : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private Image _slider;
+    [SerializeField] private float _step;
+
+    private float _health;
+
+    private void OnEnable()
+    {
+        _player.HealthChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= OnHealthChanged;
+    }
+
+    private void OnHealthChanged(int health)
+    {
+        _health = health;   
+    }
+
+    void Update()
+    {
+        _slider.fillAmount = Mathf.MoveTowards(_slider.fillAmount, _health / 100, _step);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Button _buttonAdd;
+    [SerializeField] private Button _buttonRemove;
+    [SerializeField] private int _amountHealthChanged;
+    [SerializeField] private int _maxHealth;
+
+    private int _health = 50;
+
+    public event UnityAction<int> HealthChanged;
+
+    private void Start()
+    {
+        HealthChanged?.Invoke(_health);
+    }
+
+    private void OnEnable()
+    {
+        _buttonAdd.onClick.AddListener(AddHp);
+        _buttonRemove.onClick.AddListener(RemoveHp);
+    }
+
+    private void OnDisable()
+    {
+        _buttonAdd.onClick.RemoveListener(AddHp);
+        _buttonRemove.onClick.RemoveListener(RemoveHp);
+    }
+
+    private void AddHp()
+    {
+        _health += _amountHealthChanged;
+
+        if (_health > _maxHealth)
+            _health = _maxHealth;
+
+        HealthChanged?.Invoke(_health);
+    }
+
+    private void RemoveHp()
+    {
+        _health -= _amountHealthChanged;
+
+        if (_health < 0)
+            _health = 0;
+
+        HealthChanged?.Invoke(_health);
+    }
+}
